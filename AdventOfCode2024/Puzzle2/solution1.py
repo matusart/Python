feb0e570e8974f7f786393d44ebab9ae1160f83f
Artur <artur.k.matusiak@gmail.com>
2024-12-16 16:42:41 +0100

    made a list of integers

--- AdventOfCode2024/Puzzle2/solution1.py
+++ AdventOfCode2024/Puzzle2/solution1.py
@@ -2,7 +2,21 @@
 with open("AdventOfCode2024\Puzzle2\Input2.txt", "r") as file:
   input_data = file.readlines()
 
+a = []
+for line in input_data:
+  # loop goes until end of the line. Than goes to the next line.
+  for x in line.split():
+    newInt = int(x)
+    #here the integer is added to the list a
+    a.append(newInt)  
+  print(a)
+  a.clear()
+
+
+  # I have to go through the 
+  # a.append(int(line.split()[0].strip()))
+
   #zmien dane na integers
   #wyczysc spacje
-  #
+  #Sprawdz czy dane sa wzrastajace czy malejace
 
